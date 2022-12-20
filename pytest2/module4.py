18d21dca28a677e957b934a83351cc4c0bab1675
Nugget-Meister <tremainecamacho@gmail.com>
2022-12-20 08:42:04 -0500

    testing hex reader

--- pytest2/module4.py
+++ pytest2/module4.py
@@ -1,14 +1,36 @@
-
+import binascii as bin
 
 #### The hex converter
 
 def convertHex(value):
     nug = hex(value)
 
-    buffer = nug.split('0x')
-    nug = buffer[1]
+   # buffer = nug.split('0x')
+   # nug = buffer[1]
 
     print(nug)
     print(len(nug))
 
-convertHex(28454492)
+def loadSongList():
+    songFile = open('bgm.stq', 'rb')
+    
+    fileBuffer = songFile.read()
+
+    songFile.close()
+   
+    #gameSong = fileBuffer.split(',\n')
+    #return gameSong
+
+    #print(fileBuffer)
+    #print(gameSong)
+
+    test = "S"
+    test = bin.hexlify(fileBuffer)
+    print(test)
+    
+
+
+
+
+#convertHex(284544922)
+loadSongList()
